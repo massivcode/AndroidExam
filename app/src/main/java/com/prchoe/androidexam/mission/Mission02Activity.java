61f751330973b796abce4410454eea3a2db0b614
prChoe <massivcode@gmail.com>
2015-09-02 14:49:59 +0900

    4. Mission02 - 80 바이트 초과시 Toast 메세지 출력 & 키보드 내림

--- app/src/main/java/com/prchoe/androidexam/mission/Mission02Activity.java
+++ app/src/main/java/com/prchoe/androidexam/mission/Mission02Activity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,13 +13,13 @@ import android.widget.Toast;
 
 import com.prchoe.androidexam.R;
 
-public class Mission02Activity extends AppCompatActivity implements View.OnClickListener{
+public class Mission02Activity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mSendBtn, mCloseBtn;
     private EditText mMessageEditText;
     private TextView mMessageLengthTV;
     private String message;
-    private int messageLength;
+    private int messageLength = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +31,10 @@ public class Mission02Activity extends AppCompatActivity implements View.OnClick
     }
 
     private void initView() {
-        mSendBtn = (Button)findViewById(R.id.sendBtn);
-        mCloseBtn = (Button)findViewById(R.id.closeBtn);
-        mMessageEditText = (EditText)findViewById(R.id.messageEditText);
-        mMessageLengthTV = (TextView)findViewById(R.id.messageLengthTV);
+        mSendBtn = (Button) findViewById(R.id.sendBtn);
+        mCloseBtn = (Button) findViewById(R.id.closeBtn);
+        mMessageEditText = (EditText) findViewById(R.id.messageEditText);
+        mMessageLengthTV = (TextView) findViewById(R.id.messageLengthTV);
     }
 
     private void initListener() {
@@ -62,7 +63,7 @@ public class Mission02Activity extends AppCompatActivity implements View.OnClick
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.sendBtn:
-                Toast.makeText(getApplicationContext(), message , Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.closeBtn:
                 finish();
@@ -72,8 +73,14 @@ public class Mission02Activity extends AppCompatActivity implements View.OnClick
     }
 
     private void getMessage() {
-        message = mMessageEditText.getText().toString();
-        messageLength = mMessageEditText.getText().length();
+        if (messageLength < 80) {
+            message = mMessageEditText.getText().toString();
+            messageLength = mMessageEditText.getText().length();
+        } else {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(mMessageEditText.getWindowToken(), 0);
+            Toast.makeText(getApplicationContext(), "80 바이트를 초과하였습니다.", Toast.LENGTH_SHORT).show();
+        }
 
         mMessageLengthTV.setText(messageLength + " / 80 바이트");
     }
