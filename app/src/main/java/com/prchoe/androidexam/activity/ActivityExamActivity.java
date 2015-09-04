541e87ee8a74c050b373257b323efdf5b654423a
prChoe <massivcode@gmail.com>
2015-09-04 11:19:06 +0900

    8. startActivityForResult(), onActivityResult, setResult 사용 예제 - 다른 Activity 띄우고, 결과값 세팅하여 리턴 받는 예제

--- app/src/main/java/com/prchoe/androidexam/activity/ActivityExamActivity.java
+++ app/src/main/java/com/prchoe/androidexam/activity/ActivityExamActivity.java
@@ -1,3 +1,4 @@
+
 package com.prchoe.androidexam.activity;
 
 import android.content.Intent;
@@ -5,12 +6,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.prchoe.androidexam.R;
 import com.prchoe.androidexam.interfaces.Layout;
 
 public class ActivityExamActivity extends AppCompatActivity implements Layout {
 
+    public static final int REQUEST_CODE_STRING = 1000;
     private EditText mNameET, mPhoneET;
 
     @Override
@@ -21,22 +24,39 @@ public class ActivityExamActivity extends AppCompatActivity implements Layout {
         initView();
         initListener();
 
-
     }
 
     @Override
     public void initView() {
-        mNameET = (EditText)findViewById(R.id.name_edit_text);
-        mPhoneET = (EditText)findViewById(R.id.phone_edit_text);
+        mNameET = (EditText) findViewById(R.id.name_edit_text);
+        mPhoneET = (EditText) findViewById(R.id.phone_edit_text);
     }
 
     @Override
     public void initListener() {
         findViewById(R.id.sendBtn).setOnClickListener(this);
+        findViewById(R.id.sendBtn2).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+
+
+
+        switch (v.getId()) {
+            case R.id.sendBtn:
+                btn1Click();
+                break;
+            case R.id.sendBtn2:
+                btn2Click();
+                break;
+        }
+
+
+
+    }
+
+    private void btn1Click() {
         // TargetActivity로 이동
         Intent intent = new Intent(getApplicationContext(), TargetActivity.class);
 
@@ -44,8 +64,44 @@ public class ActivityExamActivity extends AppCompatActivity implements Layout {
         intent.putExtra("name", mNameET.getText().toString());
         intent.putExtra("phone", mPhoneET.getText().toString());
 
-
         startActivity(intent);
+    }
 
+    private void btn2Click() {
+        // TargetActivity로 이동
+        Intent intent = new Intent(getApplicationContext(), TargetActivity.class);
+
+        // Data 추가
+        intent.putExtra("name", mNameET.getText().toString());
+        intent.putExtra("phone", mPhoneET.getText().toString());
+
+        // ======= 주는 곳 ========
+        //
+        // 데이터를 돌려받기 위해서는 startActivityForResult로 호출 해야 됨
+        // 리퀘스트 코드는 원하는 값을 받기 위한 약속같은 것
+        startActivityForResult(intent, REQUEST_CODE_STRING);
+    }
+
+    // ========= 받는 곳 ===========
+    //
+    // startActivityForResult로 호출 후에 데이터를 리턴 받는 곳
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 결과가 OK냐
+        if(resultCode == RESULT_OK) {
+            // RequestCode에 따라 처리를 나눔
+            if(requestCode == REQUEST_CODE_STRING) {
+                // data가 왔으면
+                if(data != null) {
+                    String result = data.getStringExtra("result");
+                    Toast.makeText(getApplicationContext(), "result : " + result, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } else {
+            Toast.makeText(getApplicationContext(), "에러", Toast.LENGTH_SHORT).show();
+        }
     }
 }
