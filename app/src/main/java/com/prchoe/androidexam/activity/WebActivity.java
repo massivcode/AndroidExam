1190e6addb8ca2bd2e90ae3e890212ce3fe5cbad
prChoe <massivcode@gmail.com>
2015-09-07 14:07:47 +0900

    13. WebView 예제

--- app/src/main/java/com/prchoe/androidexam/activity/WebActivity.java
+++ app/src/main/java/com/prchoe/androidexam/activity/WebActivity.java
@@ -2,13 +2,18 @@ package com.prchoe.androidexam.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 import com.prchoe.androidexam.R;
 
-public class WebActivity extends AppCompatActivity {
+public class WebActivity extends AppCompatActivity implements View.OnKeyListener {
 
     private WebView mWebView;
+    private EditText mAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +21,23 @@ public class WebActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web);
 
         mWebView = (WebView)findViewById(R.id.web_view);
-        mWebView.loadUrl("http://m.naver.com");
+        mAddress = (EditText)findViewById(R.id.et_web_address);
+        mAddress.setOnKeyListener(this);
+
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
     }
 
+    private void loadUrl(String url) {
+        mWebView.loadUrl(url);
+    }
 
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_ENTER) {
+            loadUrl("http://" + mAddress.getText().toString());
+            return true;
+        }
+        return false;
+    }
 }
