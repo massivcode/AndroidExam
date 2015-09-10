5c9ce7520107235e4b9a124ddb36fef725ae92df
prChoe <massivcode@gmail.com>
2015-09-10 10:14:12 +0900

    18. Main Activity 수정

--- app/src/main/java/com/prchoe/androidexam/MainActivity.java
+++ app/src/main/java/com/prchoe/androidexam/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends ListActivity {
         addItem(myData, "도전! 06: 웹브라우저 화면 구성", Mission06Activity.class);
         addItem(myData, "Extra 01: 갤러리에서 사진 선택하여 표시하기", MissionExtra01Activity.class);
         addItem(myData, "Extra 02: ListView에 Animation 적용하기", MissionExtra02Activity.class);
-        addItem(myData, "월별 캘린더 만들기", CalendarActivity.class);
+        addItem(myData, "도전! 07 & 08 : 캘린더", CalendarActivity.class);
         addItem(myData, "구글 캘린더 뷰", Calendar2Activity.class);
         addItem(myData, "화면이동 예제", ActivityExamActivity.class);
         addItem(myData, "WebView 연습", WebActivity.class);
